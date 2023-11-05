f139f571b23b607f205e00c68b727e80364209e6
RISING-CODER777 <85391175+RISING-CODER777@users.noreply.github.com>
2023-11-05 19:26:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <head>
+        <title>HR Dashboard</title>
+      </head>
+      <iframe
+        title="Fleet Management Dashboard"
+        width="1140"
+        height="541.25"
+        src="https://app.powerbi.com/reportEmbed?reportId=de5ae8f6-8060-449e-b614-a1b88eea5217&autoAuth=true&ctid=3e19468b-e065-44a1-9cde-749457855c06"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
